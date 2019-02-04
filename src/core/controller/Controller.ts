d59c9e57ae32687a821fd942f40ba201aab26f47
Roberto Honda <roberto_junior_honda@hotmail.com>
2019-02-04 17:10:13 -0300

    Fix(pretty): just format the code

--- src/core/controller/Controller.ts
+++ src/core/controller/Controller.ts
@@ -2,8 +2,8 @@ import { NextFunction, Request, Response } from "express";
 
 class Controller {
   protected handleResponse = (promise: Promise<any>, req: Request, res: Response, next: NextFunction) => {
-    return promise.then(
-      (result) => res.json(result || { message: "OK" }))
+    return promise
+      .then((result) => res.json(result || { message: "OK" }))
       .catch((error) => res.status(500) && next(error));
   }
 }
